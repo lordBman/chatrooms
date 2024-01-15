8ea5a2f39e23c854a77466873bfdc5e7c78f8e59
lordBman <okelekelenobel@gmail.com>
2024-01-15 20:29:17 +0000

    added login, but mot tested

--- website/src/pages/signin.tsx
+++ website/src/pages/signin.tsx
@@ -4,7 +4,7 @@ import { AppContext, AppContextType } from "../utils/providers";
 import { useNavigate } from "react-router-dom";
 
 const Signin = () =>{
-    const { loading, user, signin } = useContext(AppContext) as AppContextType;
+    const { loading, user, signin, login } = useContext(AppContext) as AppContextType;
     const navigate = useNavigate();
 
     const register = (event: React.FormEvent<HTMLFormElement>) =>{
@@ -17,29 +17,49 @@ const Signin = () =>{
         //alert(JSON.stringify(data.email));
     }
 
-    useEffect(()=>{
-        user && navigate("/");
-    }, [user]);
+    const loginForm = (event: React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+
+        const data = Util.extract(event.currentTarget);
+
+        login({ username: data["username"], password: data["password"] })
+
+        //alert(JSON.stringify(data.email));
+    }
+
+    useEffect(()=> user && navigate("/"), [user]);
 
     return (
-        <div style={{ }}>
-            <form onSubmit={register} method="POST">
-                <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" placeholder="Email" required/>
+        <div>
+            <div>
+                <form onSubmit={register} method="POST">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" name="email" placeholder="Email" required/>
 
+                    <label htmlFor="username">User Name</label>
+                    <input id="username" type="text" name="username" placeholder="Choose Username" required/>
+
+                    <label htmlFor="password">Enter Password</label>
+                    <input id="password" type="password" name="password" placeholder="Choose Pasowrd" required/>
+
+                    <label htmlFor="repassword">Reenter Password</label>
+                    <input id="repassword" type="password" name="repassword" placeholder="Confirm Pasowrd" required/>
+
+                    <input type="submit" value={"submit"} />
+                </form>
+            </div>
+            <div>
+            <form onSubmit={loginForm} method="POST">
                 <label htmlFor="username">User Name</label>
-                <input id="username" type="text" name="username" placeholder="Choose Username" required/>
+                <input id="username" type="text" name="username" placeholder="Enter Username" required/>
 
                 <label htmlFor="password">Enter Password</label>
-                <input id="password" type="password" name="password" placeholder="Choose Pasowrd" required/>
-
-                <label htmlFor="repassword">Reenter Password</label>
-                <input id="repassword" type="password" name="repassword" placeholder="Confirm Pasowrd" required/>
+                <input id="password" type="password" name="password" placeholder="Enter Pasowrd" required/>
 
                 <input type="submit" value={"submit"} />
             </form>
+            </div>
             { loading && <div>Loading... </div> }
-            { user && <div>{ JSON.stringify(user) }</div> }
         </div>
     );
 }
